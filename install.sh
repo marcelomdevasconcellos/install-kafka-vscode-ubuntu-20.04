207bc1e9225ea46cd766b44230e2ad1c4c2d2009
Marcelo Vasconcellos <marcelomdevasconcellos@gmail.com>
2020-09-12 10:23:13 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,14 @@ curl http://ftp.unicamp.br/pub/apache/kafka/2.6.0/kafka_2.12-2.6.0.tgz -o Downlo
 mkdir kafka
 cd kafka
 tar -xvzf ~/Downloads/kafka.tgz --strip 1
+
+sudo apt-get update -y
+sudo apt-get install openjdk-8-jdk -y
+
+sudo apt install python3-pip
+pip3 install kafka-python
+
+pip3 install Faker
+
+sudo apt install mongodb -y
+pip3 install pymongoy
